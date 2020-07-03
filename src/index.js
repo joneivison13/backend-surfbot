47f9f953aef9853c7c623dffe7590cbd6c0f6737
joneivison oliveira <joneivison355@gmail.com>
2020-07-03 15:50:43 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const app = express()
 const rotas = require('./routes')
 const body = require('body-parser')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 mongoose.connect('mongodb+srv://joneivison13:1a2a3a4a5a6a@cluster0.9efbr.mongodb.net/test', { useUnifiedTopology: true, seNewUrlParser: true }).then(() => console.log('database is ok')).catch(err => console.log(err))
 
 app.use(body.json())
 app.use(rotas)
+app.use(cors())
 
-app.listen(3333, () => console.log('iniciado com sucesso'))
+app.listen('https://backend-surfbot.herokuapp.com/', () => console.log('iniciado com sucesso'))
