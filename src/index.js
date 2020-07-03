ad330f6acd87f359241613629858a0004a7635fa
joneivison oliveira <joneivison355@gmail.com>
2020-07-03 16:44:40 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const body = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+require('dotenv').load()
+
 mongoose.connect('mongodb+srv://joneivison13:1a2a3a4a5a6a@cluster0.9efbr.mongodb.net/test', { useUnifiedTopology: true, seNewUrlParser: true }).then(() => console.log('database is ok')).catch(err => console.log(err))
 
 app.use(cors())
