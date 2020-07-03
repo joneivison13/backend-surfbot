6e51e87d92a5fd39f57242097fc0b4537d3f8015
joneivison oliveira <joneivison355@gmail.com>
2020-07-03 17:29:27 -0300

    comit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 require('dotenv').config()
 
-mongoose.connect('mongodb+srv://joneivison13:1a2a3a4a5a6a@cluster0.9efbr.mongodb.net/test', { useUnifiedTopology: true, seNewUrlParser: true }).then(() => console.log('database is ok')).catch(err => console.log(err))
+mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, seNewUrlParser: true }).then(() => console.log('database is ok')).catch(err => console.log(err))
 
 app.use(cors())
 app.use(body.json())
